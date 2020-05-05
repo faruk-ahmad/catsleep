9a9ce9804e6932e3f15f1a728edcae690f4fe059
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-05 13:45:18 +0900

    created CatSleep class in catsleep.py module

---
+++ catsleep/catsleep.py
@@ -0,0 +1,22 @@
+""" The mian module of catsleep tool """
+
+import os
+import sys
+
+
+class CatSleep():
+    """ A class that represents the basic operation of catsleep """
+
+    def __init__(self):
+        """ Initialize all the basic attributes required for catsleep """
+        pass
+
+
+    def play_beep(self):
+        """ A method to play beep sound for catsleep """
+        pass
+
+
+    def play_audio(self):
+        """ A method to play audio for catsleep """
+        pass
