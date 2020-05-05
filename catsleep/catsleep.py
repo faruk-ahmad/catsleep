b13b98aaafde275b0585bb3e2d2455224e084722
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-05 13:49:51 +0900

    updated catsleep.py

--- catsleep/catsleep.py
+++ catsleep/catsleep.py
@@ -9,14 +9,22 @@ class CatSleep():
 
     def __init__(self):
         """ Initialize all the basic attributes required for catsleep """
-        pass
+        print(f'Initializing catsleep ...')
 
 
     def play_beep(self):
         """ A method to play beep sound for catsleep """
-        pass
+        print(f'Playing beep ...')
 
 
     def play_audio(self):
         """ A method to play audio for catsleep """
-        pass
+        print(f'Playing audio ...')
+
+
+
+
+if __name__ == '__main__':
+    catsleep = CatSleep()
+    catsleep.play_beep()
+    catsleep.play_audio()
