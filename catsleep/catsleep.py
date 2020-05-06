c9465f4f42646708822c98f9fc0543ac5d25fa9e
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-06 15:42:15 +0900

    added dynamic database loading method, selecting audio, beep, text dynamically and randomly, text message and audio are aligned

--- catsleep/catsleep.py
+++ catsleep/catsleep.py
@@ -3,19 +3,24 @@
 import os
 import sys
 import time
+import json
 
 sys.path.append('..')
 from catsleep.utils import Utility
 from catsleep.config import Config
 
+DEBUG = False
 
 class CatSleep():
     """ A class that represents the basic operation of catsleep """
 
     def __init__(self):
         """ Initialize all the basic attributes required for catsleep """
-        print(f'Initializing catsleep ...')
+        if DEBUG:
+            print(f'Initializing catsleep ...')
         self.util = Utility()
+        if DEBUG:
+            print('Cat Sleep is running.')
 
 
     def play_beep(self, beep_path='./catsleep/default_notification.ogg'):
@@ -42,33 +47,67 @@ class CatSleep():
             self.util.show_text("Error!", "Something went wrong with the text notification!")
 
     
+    def load_database(self):
+        """ A method to load database files for audio, beep, texts from data.json file """
+        database_path = "./catsleep/data.json"
+        try:
+            with open(database_path, 'r') as rf:
+                data = json.load(rf)
+            return data
+        except FileNotFoundError:
+            if DEBUG:
+                print("Databse file not found.")
+            return {}
+
+
     def catsleep_control(self):
         """ A method to control the interval and frequency of alarm """
+        #Show a notification that catsleep is running and will send notification for break
+        self.display_notification("Cat Sleep", "Cat Sleep is running and will send notification to take break.")
+
+        #load the databse file for audio, text and beep sound
+        data = self.load_database()
+        if DEBUG:
+            print(f'Databse files: {data}')
+
         while True:
             try:
                 # try to get configurations
                 conf = Config()
                 configs = conf.get_user_config()
 
+                #get selected audio, text and beep for this specifi notification
+                audio_path, beep_path, text_message = self.util.select_beep_audio_text(data)
+                if DEBUG:
+                    print(f'audio path: {audio_path} - beep path: {beep_path} - text message: {text_message}')
+                #wait for certain period before next alarm
+                time.sleep(configs['interval'])
+
                 for alarm in range(configs['frequency']):
                     #check if beep playing is set as true or not
                     #first play a beep sound, different beep at different time, need to make dynamic
                     if configs['play_beep'].lower() == "yes":
-                        self.play_beep()
+                        if beep_path is not None:
+                            self.play_beep(beep_path)
+                        else:
+                            self.play_beep()
                     
                     #check if showing text is set as true or not
                     #then display the text notification, different text at different time, need to make dynamic
                     if configs['show_text'].lower() == "yes":
-                        self.display_notification("Take Break!", "Hey! you should take a break.")
+                        if text_message is not None:
+                            self.display_notification("Take Break!", text_message)
+                        else:
+                            self.display_notification("Take Break!", "Hey, you should take a break")
                     
                     #check if playing audio message is set as true or not
                     #then play a voice message, different message at different time, need to make dynamic
                     if configs['play_audio'].lower() == "yes":
-                        self.play_audio("./catsleep/audio/take_break_audio_1.wav")
+                        if audio_path is not None:
+                            self.play_audio(audio_path)
+                        else:
+                            self.play_audio("./catsleep/audio/take_break_audio_1.wav")
                     time.sleep(configs['frequency_interval'])
-
-                #wait for certain period before next alarm
-                time.sleep(configs['interval'])
             except Exception as e:
                 self.util.show_text("Error!", "Something went wrong!")
 
--- catsleep/utils.py
+++ catsleep/utils.py
@@ -1,12 +1,14 @@
 """ A utility module that maintains the audio message and text message notification """
 
 import os
+import random
 
 class Utility():
     """ A class that holds the method for playing audio message and text message """
     def __init__(self):
         self.text_error_title = "Error Occured!"
         self.text_error_message = "Something wrong happened with the text notification!"
+        random.seed(22)
 
     def show_text(self, title, message):
         """ A method to display text message as system notification """
@@ -22,3 +24,29 @@ class Utility():
             os.system('paplay ' + audio_path)
         except FileNotFoundError:
             self.show_text("Audio file not found!")
+
+
+    def select_beep_audio_text(self, data):
+        """ A method to select a random beep, audio and respective text transcript of the selected audio """
+        try:
+            audio_data = data['audio']
+            beep_data = data['beep']
+            text_data = data['texts']
+
+            audio_index = random.randint(1, len(audio_data))
+            text_index = audio_index
+            beep_index = random.randint(1, len(beep_data))
+
+            root_path = './catsleep'
+
+            audio_temp_path = 'audio_' + str(audio_index)
+            beep_temp_path = 'beep_' + str(beep_index)
+            text_temp_path = 'text_' + str(text_index)
+
+            audio_path = os.path.join(root_path, audio_data[audio_temp_path])
+            beep_path = os.path.join(root_path, beep_data[beep_temp_path])
+            text_message = text_data[text_temp_path]
+
+            return audio_path, beep_path, text_message
+        except Exception as e:
+            return None, None, None
